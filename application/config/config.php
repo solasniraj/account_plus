698daf425aa75d435724ad0630bfd84017ca662a
jonasahtserhs <jonasahtserhs@gmail.com>
2016-08-15 12:03:09 +0545

    config base url change

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/account/';
+$config['base_url'] = 'http://localhost/account_plus/';
 
 /*
 |--------------------------------------------------------------------------
