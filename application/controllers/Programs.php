f099311409db6e68dff4a3829b927f3d81214073
bhomnath <bhomnath@salyani.com.np>
2016-08-23 14:52:26 +0545

    program added and working to add sub ledger of a program

--- application/controllers/Programs.php
+++ application/controllers/Programs.php
@@ -42,10 +42,7 @@
       {
         $user_id=$this->session->userdata('user_id');
        $this->load->library('form_validation');
-       $this->form_validation->set_rules('code', 'code', 'trim|required|callback_xss_clean|max_length[200]');
-       $this->form_validation->set_rules('programName', 'program Name', 'trim|required|callback_xss_clean|max_length[200]');
-       $this->form_validation->set_rules('programBudget', 'program Budget', 'trim|required|callback_xss_clean|max_length[200]');
-       $this->form_validation->set_rules('category', 'category', 'trim|required|callback_xss_clean|max_length[200]');
+       $this->form_validation->set_rules('programName', 'Program Name', 'trim|required|callback_xss_clean|max_length[200]');
        $this->form_validation->set_error_delimiters('<div class="form-errors">', '</div>'); 
 
        if ($this->form_validation->run() == FALSE)
@@ -53,14 +50,10 @@
         $this->addProgram();
       }
       else 
-      {
-        $code=$this->input->post('code');
-        $programName=$this->input->post('programName');
-        $programBudget=$this->input->post('programBudget');
-        $category=$this->input->post('category');
-        
+      {       
+        $programName=$this->input->post('programName');        
         $this->load->model('program_model');
-        $result=$this->program_model->insert_programm_listing($user_id,$code, $programName, $programBudget, $category);
+        $result=$this->program_model->insert_programm_listing($user_id, $programName);
         if($result)
         {
          $this->session->set_flashdata('flashMessage', 'Programm added successfully');
@@ -106,6 +99,65 @@ public function editProgram($id=NULL)
   } else {
     redirect('login/index/?url=' . $url, 'refresh');
   }
+}
+
+public function createSubLedger($id=NULL)
+{
+    $url = current_url();
+  if ($this->session->userdata('logged_in') == true) {
+
+       $this->load->view('dashboard/templates/header');
+   $this->load->view('dashboard/templates/sideNavigation');
+   $this->load->view('dashboard/templates/topHead');
+   $this->load->view('dashboard/program/addSubLedger');
+   $this->load->view('dashboard/templates/footer');  
+      
+      
+  } else {
+    redirect('login/index/?url=' . $url, 'refresh');
+  }
+}
+
+
+public function addSubLedger()
+{
+      { 
+      $url = current_url();
+      if ($this->session->userdata('logged_in') == true) 
+      {
+        $user_id=$this->session->userdata('user_id');
+       $this->load->library('form_validation');
+       $this->form_validation->set_rules('subLedgerName', 'Sub-ledger Name', 'trim|required|callback_xss_clean|max_length[200]');
+       $this->form_validation->set_error_delimiters('<div class="form-errors">', '</div>'); 
+
+       if ($this->form_validation->run() == FALSE)
+       {
+        $this->addProgram();
+      }
+      else 
+      {       
+        $programName=$this->input->post('programName');        
+        $this->load->model('program_model');
+        $result=$this->program_model->insert_programm_listing($user_id, $programName);
+        if($result)
+        {
+         $this->session->set_flashdata('flashMessage', 'Programm added successfully');
+         return redirect('programs/programListing');
+       }
+       else 
+       {
+        $this->session->set_flashdata('flashMessage', 'error occur while adding programm');
+
+        return redirect('programs/programListing');
+      }
+
+
+    }
+  }
+  else {
+   return   redirect('login/index/?url=' . $url, 'refresh');
+ }
+}
 }
 
 public function xss_clean($str)
--- application/views/dashboard/program/addSubLedger.php
+++ application/views/dashboard/program/addSubLedger.php
@@ -1,6 +1,6 @@
             <div id="page-wrapper">
                 <div class="graphs">
-                    <h3 class="blank1">Add Program</h3>
+                    <h3 class="blank1">Add Sub Ledger to Program </h3>
                      <style>
      /*sanoj custom csss */
                 .form-errors 
@@ -17,14 +17,14 @@
                 </style>
                     <div class="tab-content">
                         <div class="tab-pane active" id="horizontal-form">
-                            <?php echo form_open_multipart('programs/addnewProgram', array('id' => '','class'=>'form-horizontal', 'novalidate'=>'novalidate'));?>
+                            <?php echo form_open_multipart('programs/addSubLedger', array('id' => '','class'=>'form-horizontal', 'novalidate'=>'novalidate'));?>
                             
 
                             <div class="form-group">
-                                <label for="programName" class="col-sm-2 control-label">Program Name</label>
+                                <label for="subLedgerName" class="col-sm-2 control-label">Sub-ledger Name</label>
                                 <div class="col-sm-8">
-                                    <input type="text" value="<?php echo set_value('programName'); ?>" class="form-control1" id="programName" name="programName" placeholder="Enter program name">
-                                    <?php echo form_error('programName'); ?>
+                                    <input type="text" value="<?php echo set_value('subLedgerName'); ?>" class="form-control1" id="subLedgerName" name="subLedgerName" placeholder="Enter sub ledger name">
+                                    <?php echo form_error('subLedgerName'); ?>
                                 </div>
                             </div>
 
