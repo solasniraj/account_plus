8c9c9d33bad06af89ce7d53269af38a849408646
solas niraj <niraaj_chatri143@gmail.com>
2016-09-09 13:19:24 +0545

    change the view of journal list

---
+++ application/views/dashboard/bank/viewJournal.php
@@ -0,0 +1,55 @@
+                                    <div class="table-responsive">
+                                        <table class="tablee">
+                                            <tbody>
+
+                                                <tr>
+                                                 <td class="col-md-1"><b>Journal no</b></td>
+                                                    <td class="col-md-1"><b>Journal type</b></td>
+                                                    <td class="col-md-1"><b>Date</b></td>
+                                                    <td class="col-md-1">Bank Balance</td>
+                                                    </tr>
+
+
+                                                    <tr>
+                                                    <td id="journalNo">
+                                                            <input type="text" id="journal" class="form-control" >
+                                                        </td>
+                                                       <td><select class="form-control" id="programsList" onchange="whenProgramIsSelected(this)">
+                                                  <option value="">Select Types</option>
+                                                         </select></td>    
+                                                        
+                                        <td><input class="form-control" id="datepicker" type="text" placeholder="Day/Month/Year"></td>
+                                                        <td id="journalNo">
+                                                            <input type="text" id="journal" class="form-control" >
+                                                        </td>
+
+                                                        
+                                                    </tr>
+                                                    </tbody>
+                                                    </table>
+                                                    </div>
+
+
+                                                    <br>
+
+                                                    <div class="tab-content">
+<table class="table table-striped table-bordered table-condensed">
+<thead>
+<tr>
+<th>Code#</th>
+<th>A/C Head </th>
+<th>Sub-ledger</th>
+<th>Donar-list</th>
+<th>Ledger type</th>
+<th>Description</th>
+<th>Debit</th>
+<th>Credit</th>
+<th>Cheque Number</th>
+</tr>
+</thead>
+</table>
+</div>
+
+
+
+
--- application/views/printPreview/preview/transaction/singleJournalEntry.php
+++ application/views/printPreview/preview/transaction/singleJournalEntry.php
@@ -31,14 +31,10 @@
                             <th>Nepal Government</th>
                             <th><input id="datepicker" class="form-control" type="text" placeholder="Day/Month/Year" name="datepicker"></th>
                             <th>Amount(Rs)</th>
-                            <th><a href="http://localhost/account_plus/programs/edit/5">Edit</a>
-/ </th>
-                            <th><a href="http://localhost/account_plus/programs/edit/5">Delete</a>
-/ </th>
-                            <th><a href="http://localhost/account_plus/programs/edit/5">Post</a>
-/ </th>
-                            <th><a href="http://localhost/account_plus/programs/edit/5">Print</a>
-/ </th>
+                            <th><a href="http://localhost/account_plus/programs/edit/5">Edit</a> </th>
+                            <th><a href="http://localhost/account_plus/programs/edit/5">Delete</a></th>
+                            <th><a href="http://localhost/account_plus/programs/edit/5">Post</a></th>
+                            <td><a href="<?php echo base_url() . 'transaction/preview/' . $tGLList->gl_no; ?>">Preview</a></td>
                         </tr>
 
                             
