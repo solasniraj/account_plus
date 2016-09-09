88fb5ef4eb3d6d9529e4c35cb4b0698470b31db7
bhomnath <bhomnath@salyani.com.np>
2016-09-09 11:09:22 +0545

    database updated

--- _database/account.sql
+++ _database/account.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 01, 2016 at 10:40 पूर्वाह्न
+-- Generation Time: Sep 09, 2016 at 07:23 AM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -19,6 +19,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `account`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bank_account_type`
+--
+
+CREATE TABLE IF NOT EXISTS `bank_account_type` (
+  `id` int(11) NOT NULL,
+  `bank_account_type` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `bank_account_type`
+--
+
+INSERT INTO `bank_account_type` (`id`, `bank_account_type`, `status`) VALUES
+(1, 'Savings Account', '1'),
+(2, 'Call Account', '1'),
+(3, 'Current Account', '1'),
+(4, 'Fixed Account', '1');
 
 -- --------------------------------------------------------
 
@@ -40,14 +62,14 @@ CREATE TABLE IF NOT EXISTS `bank_info` (
   `committee_id` varchar(255) DEFAULT NULL,
   `user_id` varchar(255) DEFAULT NULL,
   `status` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `bank_info`
 --
 
 INSERT INTO `bank_info` (`id`, `account_code`, `account_type`, `bank_account_name`, `bank_name`, `bank_address`, `bank_account_number`, `bank_phone_no`, `last_reconciled_date`, `ending_reconcile_balance`, `committee_id`, `user_id`, `status`) VALUES
-(1, NULL, NULL, NULL, 'Himalayan Bank Limited', 'Narayangarh, Chitwan', '98452141409845214140', '056-533977', NULL, 5000, NULL, NULL, 'Active');
+(5, '10101', '1', 'Dilip Poudel', 'Himalayan Bank Limited', 'Narayangarh, Chitwan', '98452141409845214140', '056-533977', NULL, NULL, NULL, NULL, '1');
 
 -- --------------------------------------------------------
 
@@ -69,19 +91,14 @@ CREATE TABLE IF NOT EXISTS `bank_trans_info` (
   `person_id` tinyblob,
   `reconciled` date DEFAULT NULL,
   `bank_id` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `bank_trans_info`
 --
 
 INSERT INTO `bank_trans_info` (`id`, `type`, `trans_no`, `bank_act`, `ref`, `trans_date`, `amount`, `dimension_id`, `dimension2_id`, `person_type_id`, `person_id`, `reconciled`, `bank_id`) VALUES
-(1, 0, 1, '1', '1', '2014-12-31', -312, 0, 0, 0, '', NULL, NULL),
-(2, 0, 2, '1', '1', '2015-01-01', 312, 0, 0, 0, '', NULL, NULL),
-(3, 0, 3, '1', '2', '2014-12-31', -500, 0, 0, 0, '', NULL, NULL),
-(4, 0, 4, '1', '3', '2014-12-31', 500, 0, 0, 0, '', NULL, NULL),
-(5, 0, 5, '1', '4', '2014-12-31', 1000, 0, 0, 0, '', NULL, NULL),
-(6, 0, 6, '2', '5', '2014-12-31', 100, 0, 0, 0, '', NULL, NULL);
+(4, 0, 4, '1', '3', '2014-12-31', 500, 0, 0, 0, '', NULL, '2');
 
 -- --------------------------------------------------------
 
@@ -120,34 +137,35 @@ CREATE TABLE IF NOT EXISTS `chart_master` (
   `account_name` varchar(255) DEFAULT NULL,
   `account_status` varchar(100) DEFAULT NULL,
   `chart_class_id` varchar(20) DEFAULT NULL,
-  `program_id` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+  `program_id` varchar(20) DEFAULT NULL,
+  `bank_id` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `chart_master`
 --
 
-INSERT INTO `chart_master` (`id`, `account_code`, `account_code2`, `account_name`, `account_status`, `chart_class_id`, `program_id`) VALUES
-(1, '100', '', 'Cash Account', '0', '1', NULL),
-(2, '101', '', 'Bank Account', '0', '1', NULL),
-(3, '102', '', 'Bank Account', '0', '1', NULL),
-(4, '103', '', 'Personal Advance', '0', '1', NULL),
-(5, '104', '', 'Official Advance', '0', '1', NULL),
-(6, '105', '', 'Accounts Receivable', '0', '1', NULL),
-(7, '201', '', 'Accounts Payable', '0', '2', NULL),
-(8, '202', '', 'TDS Payable', '0', '2', NULL),
-(9, '301', '', 'Plantation', '0', '3', NULL),
-(10, '302', '', 'Grassland Management', '0', '3', NULL),
-(11, '303', '', 'AAAAAAAA Income', '0', '3', NULL),
-(12, '401', '', 'Salary Expenses', '0', '4', NULL),
-(13, '402', '', 'TADA Expenses', '0', '4', NULL),
-(14, '403', '', 'Salary Expenses', '0', '4', NULL),
-(15, '404', '', 'Rental Expenses', '0', '4', NULL),
-(16, '405', '', 'Repair &amp; Maintenance Expenses', '0', '4', NULL),
-(17, '406', '', 'Email, Internet, Telephone &amp; Postage Expenses', '0', '4', NULL),
-(18, '407', '', 'AAAAAAAAAA Expenses', '0', '4', NULL),
-(19, '106', NULL, 'Sanoj System', 'Active', '1', '16'),
-(20, '107', NULL, 'Grassland Management System', 'Active', '1', '17');
+INSERT INTO `chart_master` (`id`, `account_code`, `account_code2`, `account_name`, `account_status`, `chart_class_id`, `program_id`, `bank_id`) VALUES
+(1, '100', '', 'Cash Account', '0', '1', NULL, NULL),
+(2, '101', '', 'Bank Account', '0', '1', NULL, NULL),
+(3, '102', '', 'Bank Account', '0', '1', NULL, NULL),
+(4, '103', '', 'Personal Advance', '0', '1', NULL, NULL),
+(5, '104', '', 'Official Advance', '0', '1', NULL, NULL),
+(6, '105', '', 'Accounts Receivable', '0', '1', NULL, NULL),
+(7, '201', '', 'Accounts Payable', '0', '2', NULL, NULL),
+(8, '202', '', 'TDS Payable', '0', '2', NULL, NULL),
+(9, '301', '', 'Plantation', '0', '3', NULL, NULL),
+(10, '302', '', 'Grassland Management', '0', '3', NULL, NULL),
+(11, '303', '', 'AAAAAAAA Income', '0', '3', NULL, NULL),
+(12, '401', '', 'Salary Expenses', '0', '4', NULL, NULL),
+(13, '402', '', 'TADA Expenses', '0', '4', NULL, NULL),
+(14, '403', '', 'Salary Expenses', '0', '4', NULL, NULL),
+(15, '404', '', 'Rental Expenses', '0', '4', NULL, NULL),
+(16, '405', '', 'Repair &amp; Maintenance Expenses', '0', '4', NULL, NULL),
+(17, '406', '', 'Email, Internet, Telephone &amp; Postage Expenses', '0', '4', NULL, NULL),
+(18, '407', '', 'AAAAAAAAAA Expenses', '0', '4', NULL, NULL),
+(22, NULL, NULL, 'Dilip Poudel', 'Active', '101', NULL, '5'),
+(23, '304', NULL, 'Grassland Management System', 'Active', '3', '19', NULL);
 
 -- --------------------------------------------------------
 
@@ -182,17 +200,7 @@ CREATE TABLE IF NOT EXISTS `donar_budget_info` (
   `donar_id` varchar(50) DEFAULT NULL,
   `donation_amount` varchar(100) DEFAULT NULL,
   `program_id` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `donar_budget_info`
---
-
-INSERT INTO `donar_budget_info` (`id`, `donar_id`, `donation_amount`, `program_id`) VALUES
-(1, NULL, '50000', '17'),
-(2, '4', '50000', '17'),
-(3, '3', '15000', '17'),
-(4, '4', '500', '17');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -207,18 +215,22 @@ CREATE TABLE IF NOT EXISTS `donar_info` (
   `donar_contact_no` varchar(255) DEFAULT NULL,
   `donar_code` varchar(100) DEFAULT NULL,
   `donar_email` varchar(100) DEFAULT NULL,
+  `contact_person` varchar(100) DEFAULT NULL,
+  `contact_person_cell_no` varchar(50) DEFAULT NULL,
   `committee_id` varchar(255) DEFAULT NULL,
   `user_id` varchar(255) DEFAULT NULL,
   `status` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `donar_info`
 --
 
-INSERT INTO `donar_info` (`id`, `donar_name`, `donar_address`, `donar_contact_no`, `donar_code`, `donar_email`, `committee_id`, `user_id`, `status`) VALUES
-(3, 'WWF', 'America', '50000', '01', 'bhomnath@salyani.com.np', NULL, NULL, 'Active'),
-(4, 'TAAL', 'Bharatpur, Chitwan', '50000', '02', 'bhomnath@salyani.com.np', NULL, NULL, 'Active');
+INSERT INTO `donar_info` (`id`, `donar_name`, `donar_address`, `donar_contact_no`, `donar_code`, `donar_email`, `contact_person`, `contact_person_cell_no`, `committee_id`, `user_id`, `status`) VALUES
+(3, 'WWF', 'America', '50000', '01', 'bhomnath@salyani.com.np', NULL, NULL, NULL, NULL, 'Active'),
+(4, 'TAAL', 'Bharatpur, Chitwan', '50000', '02', 'bhomnath@salyani.com.np', NULL, NULL, NULL, NULL, 'Active'),
+(5, 'sdhjkf dsbkj', 'bkjsdbfk fsjkd', 'dskj fkjs sd f', 'sdkjf kjsd fs', 'kdsjbfjs jksd kj', NULL, NULL, NULL, NULL, 'Active'),
+(6, 'TAAL', 'America', '056-533977', NULL, 'bhomnath@salyani.com.np', 'dhfjksf jksbdkj', 'bkjbskjdf sdkj', NULL, NULL, '1');
 
 -- --------------------------------------------------------
 
@@ -233,14 +245,35 @@ CREATE TABLE IF NOT EXISTS `fiscal_year_info` (
   `end_date` varchar(255) DEFAULT NULL,
   `committee_name` text,
   `status` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `fiscal_year_info`
 --
 
 INSERT INTO `fiscal_year_info` (`id`, `fiscal_year`, `begin_date`, `end_date`, `committee_name`, `status`) VALUES
-(3, '2072/2073', NULL, NULL, 'TAAL', 'Active');
+(3, '2072/2073', '2072/04/01', '2073/03/30', 'TAAL', 'Active'),
+(4, '2073/2074', '2073/04/01', '2074/03/01', 'ASDF', 'Active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gl_trans_comment_details`
+--
+
+CREATE TABLE IF NOT EXISTS `gl_trans_comment_details` (
+  `id` int(11) NOT NULL,
+  `trans_no` varchar(255) DEFAULT NULL,
+  `detailed_comment` text,
+  `summary_comment` text
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `gl_trans_comment_details`
+--
+
+INSERT INTO `gl_trans_comment_details` (`id`, `trans_no`, `detailed_comment`, `summary_comment`) VALUES
+(3, '1', 'jhg', 'jhghjs');
 
 -- --------------------------------------------------------
 
@@ -249,32 +282,29 @@ INSERT INTO `fiscal_year_info` (`id`, `fiscal_year`, `begin_date`, `end_date`, `
 --
 
 CREATE TABLE IF NOT EXISTS `gl_trans_info` (
-  `counter` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `gl_no` varchar(255) DEFAULT NULL,
   `type` smallint(6) NOT NULL DEFAULT '0',
   `type_no` bigint(16) NOT NULL DEFAULT '1',
   `tran_date` date NOT NULL DEFAULT '0000-00-00',
-  `account` varchar(15) NOT NULL DEFAULT '',
-  `memo_` tinytext NOT NULL,
+  `account_code` varchar(255) DEFAULT NULL,
+  `account_head` varchar(255) DEFAULT '',
+  `sub_ledger` varchar(255) DEFAULT NULL,
+  `donor_id` varchar(255) DEFAULT NULL,
+  `ledger_type` varchar(255) DEFAULT NULL,
+  `memo` tinytext,
   `amount` double NOT NULL DEFAULT '0',
-  `dimension_id` int(11) NOT NULL DEFAULT '0',
-  `dimension2_id` int(11) NOT NULL DEFAULT '0',
-  `person_type_id` int(11) DEFAULT NULL,
-  `person_id` tinyblob
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+  `cheque_no` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `gl_trans_info`
 --
 
-INSERT INTO `gl_trans_info` (`counter`, `type`, `type_no`, `tran_date`, `account`, `memo_`, `amount`, `dimension_id`, `dimension2_id`, `person_type_id`, `person_id`) VALUES
-(1, 0, 1, '2014-12-31', '1060', '', -312, 0, 0, NULL, NULL),
-(2, 0, 1, '2014-12-31', '2150', '', 312, 0, 0, NULL, NULL),
-(3, 0, 2, '2015-01-01', '1060', '', 312, 0, 0, NULL, NULL),
-(4, 0, 2, '2015-01-01', '2150', '', -312, 0, 0, NULL, NULL),
-(5, 0, 3, '2014-12-31', '2620', '', 500, 0, 0, NULL, NULL),
-(6, 0, 3, '2014-12-31', '1060', '', -500, 0, 0, NULL, NULL),
-(7, 0, 4, '2014-12-31', '2620', '', -500, 0, 0, NULL, NULL),
-(17, 0, 4, '2014-12-31', '1060', '', 500, 0, 0, NULL, NULL);
+INSERT INTO `gl_trans_info` (`id`, `gl_no`, `type`, `type_no`, `tran_date`, `account_code`, `account_head`, `sub_ledger`, `donor_id`, `ledger_type`, `memo`, `amount`, `cheque_no`, `status`) VALUES
+(24, '1', 0, 1, '0000-00-00', '23', 'Grassland Management System', '', '', '', 'sajdh', 500, '', '1'),
+(25, '1', 0, 1, '0000-00-00', '2', 'Bank Account', '', '', '', 'asdf', -500, '', '1');
 
 -- --------------------------------------------------------
 
@@ -292,16 +322,14 @@ CREATE TABLE IF NOT EXISTS `programs_list` (
   `subledger_id` text,
   `fiscal_year` varchar(255) DEFAULT NULL,
   `donor_id` varchar(250) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `programs_list`
 --
 
 INSERT INTO `programs_list` (`id`, `program_code`, `program_name`, `user_id`, `status`, `committee_id`, `subledger_id`, `fiscal_year`, `donor_id`) VALUES
-(15, '1', 'Grassland Management System', 1, 'Active', NULL, NULL, NULL, NULL),
-(16, '02', 'Sanoj System', 1, 'Active', NULL, NULL, NULL, NULL),
-(17, '02', 'Grassland Management System', 1, NULL, NULL, '3<##>4<##>5<##>6<##>7<##>8<##>9<##>10<##>11', NULL, NULL);
+(19, '01', 'Grassland Management System', 1, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -315,18 +343,7 @@ CREATE TABLE IF NOT EXISTS `subledger_info` (
   `subledger_code` varchar(255) DEFAULT NULL,
   `subledger_status` varchar(255) DEFAULT NULL,
   `program_id` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `subledger_info`
---
-
-INSERT INTO `subledger_info` (`id`, `subledger_name`, `subledger_code`, `subledger_status`, `program_id`) VALUES
-(7, 'dilip', '01', 'active', '17'),
-(8, 'HnB', '02', 'active', '17'),
-(9, 'Hom Nath', '03', 'active', '17'),
-(10, 'Niroj', '04', 'active', '17'),
-(11, 'sanoj', '05', 'active', '17');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -356,6 +373,12 @@ INSERT INTO `user_info` (`id`, `user_name`, `password`, `status`, `user_type`, `
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `bank_account_type`
+--
+ALTER TABLE `bank_account_type`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `bank_info`
@@ -410,15 +433,19 @@ ALTER TABLE `fiscal_year_info`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `gl_trans_comment_details`
+--
+ALTER TABLE `gl_trans_comment_details`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `gl_trans_info`
 --
 ALTER TABLE `gl_trans_info`
-  ADD PRIMARY KEY (`counter`),
+  ADD PRIMARY KEY (`id`),
   ADD KEY `Type_and_Number` (`type`,`type_no`),
-  ADD KEY `dimension_id` (`dimension_id`),
-  ADD KEY `dimension2_id` (`dimension2_id`),
   ADD KEY `tran_date` (`tran_date`),
-  ADD KEY `account_and_tran_date` (`account`,`tran_date`);
+  ADD KEY `account_and_tran_date` (`account_head`,`tran_date`);
 
 --
 -- Indexes for table `programs_list`
@@ -443,15 +470,20 @@ ALTER TABLE `user_info`
 --
 
 --
+-- AUTO_INCREMENT for table `bank_account_type`
+--
+ALTER TABLE `bank_account_type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
 -- AUTO_INCREMENT for table `bank_info`
 --
 ALTER TABLE `bank_info`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `bank_trans_info`
 --
 ALTER TABLE `bank_trans_info`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `chart_class`
 --
@@ -461,7 +493,7 @@ ALTER TABLE `chart_class`
 -- AUTO_INCREMENT for table `chart_master`
 --
 ALTER TABLE `chart_master`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24;
 --
 -- AUTO_INCREMENT for table `committee_info`
 --
@@ -471,32 +503,37 @@ ALTER TABLE `committee_info`
 -- AUTO_INCREMENT for table `donar_budget_info`
 --
 ALTER TABLE `donar_budget_info`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `donar_info`
 --
 ALTER TABLE `donar_info`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `fiscal_year_info`
 --
 ALTER TABLE `fiscal_year_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `gl_trans_comment_details`
+--
+ALTER TABLE `gl_trans_comment_details`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `gl_trans_info`
 --
 ALTER TABLE `gl_trans_info`
-  MODIFY `counter` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=26;
 --
 -- AUTO_INCREMENT for table `programs_list`
 --
 ALTER TABLE `programs_list`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=18;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT for table `subledger_info`
 --
 ALTER TABLE `subledger_info`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `user_info`
 --
