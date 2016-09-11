7cae2fb42b33b84fdcbb9e3f1237f6895a97d87d
bhomnath <bhomnath@salyani.com.np>
2016-09-11 13:39:31 +0545

    working on journal views for print and download

--- application/views/printPreview/preview/transaction/singleJournalEntry.php
+++ application/views/printPreview/preview/transaction/singleJournalEntry.php
@@ -1,18 +1,34 @@
-
+<?php if(!empty($singleGLDetails)){ 
+     foreach($singleGLDetails as $glDetails){
+      $gLDate = $glDetails->tran_date;
+      $voucherNo = $glDetails->gl_no;
+      $summary = $glDetails->summary_comment;
+      $details = $glDetails->detailed_comment;
+     }
+        
+    ?>
     <div class="container">
+        
+        
+        <?php if(!empty($committeeInfo)){ foreach ($committeeInfo as $cLists){ ?>
         <div class="top text-center" style="margin-top:22px;margin-bottom:10px;">
-           <img src="http://localhost/account_plus/image/watersplash.png" img-align="top" alt="images" style= "width:40px; height:40px;">
-           <br>
-           <h2 style="display:inline;"> GDB Nepal government Ltd </h2>
+           <!--<img src="" img-align="top" alt="images" style= "">-->
+           
+           <h2><?php echo $cLists->committee_name; ?></h2>
+           <h4><?php echo $cLists->address; ?></h4>
+           <p><strong>Ph : <?php echo $cLists->phone; ?></strong></p>
+     </div>
+        <?php }} ?>
            <span class="text-right pull-right">
                <a href="<?php echo base_url() . 'preview/jounalView/12345-FY2016-00001';?>"><button id="btnDownload" class="btn btn-primary">Download</button></a>&nbsp;&nbsp;
         <a href="<?php echo base_url() . 'printview/printJoural/12345-FY2016-00001';?>"> <button id="print" class="btn btn-primary" >print</button></a>
          </span>
-     </div>
+
     </div>
     <div id="page-wrapper">
         <div class="graphs">
             <div class="xs tabls">
+                
                 <div data-example-id="simple-responsive-table" class="bs-example4">
                       <!-- priview first table of singleJournla entry -->
 
@@ -22,75 +38,62 @@
                             <tr>
                                 <center>
                                     <th>General Ledger Transaction Details</th>
-                                    <th>Cheque Number</th>
-                                    <th>Reference</th>
-                                    <th>Date </th>
-                                    <th>Person/item </th>
+                                    <th>Date</th>
+                                    <th>Summary of Transaction</th>
+                                    <th>Details of Transaction</th>
                                 </center>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <th>Journal Entry #14 </th>
-                                <th>13</th>
-                                <th><input id="datepicker" class="form-control" type="text" placeholder="Day/Month/Year" name="datepicker"></th>
-                                <th>&nbsp</th>
+                                <td>Journal Entry : <?php echo $voucherNo; ?> </td>
+                                <td><?php echo $gLDate; ?></td>
+                                <td><?php echo $summary; ?></td>
+                                <td><?php echo $details; ?></td>
 
                             </tr>
                         </tbody>
                     </table>
                 </div>
                 <!-- second table for singleJournalEntry  -->
-
+                
                 <div class="table-responsive">
                     <table class="table table-bordered">
                         <thead>
                             <tr>
                                 <center>
                                     <th>Account code</th>
-                                    <th>Account name</th>
+                                    <th>Account head</th>
                                     <th>Sub-ledger </th>
-                                    <th>Ledger type</th>
                                     <th>Donar name</th>
+                                    <th>Ledger type</th>
+                                    <th>Description</th>
                                     <th>Debit</th>
                                     <th>Credit</th>
-                                    <th>Description</th>
-                                    <th>Demension </th>
-                                    <th>Memo</th>
+                                    <th>Cheque No.</th>
+                                    
                                 </center>
                             </tr>
                         </thead>
                         <tbody>
+                            <?php foreach ($singleGLDetails as $gLList){ ?>
                             <tr>
-                                <th>&nbsp</th>
-                                <th>&nbsp</th>                           
-                                <th>&nbsp</th>
-                                <th>&nbsp</th>
-                                <th>&nbsp</th>
-                                <th>&nbsp</th>
-                                <th>&nbsp</th>
-                                <th>&nbsp</th>
-                                <th>&nbsp</th>
-                                <th>&nbsp</th>
+                                <th><?php echo $gLList->account_code; ?></th>
+                                <th><?php echo $gLList->account_head; ?></th>                           
+                                <th><?php echo $gLList->sub_ledger; ?></th>
+                                <th><?php echo $gLList->donor_id; ?></th>
+                                <th><?php echo $gLList->ledger_type; ?></th>
+                                <th><?php echo $gLList->memo; ?></th>
+                                <th><?php echo $gLList->amount; ?></th>
+                                <th><?php echo $gLList->amount; ?></th>
+                                <th><?php echo $gLList->cheque_no; ?></th>
 
 
 
                             </tr>
-                            <tbody>
-                                <tr>
-                                    <th>&nbsp</th>
-                                <th>&nbsp</th>                           
-                                <th>&nbsp</th>
-                                <th>&nbsp</th>
-                                <th>&nbsp</th>
-                                <th>&nbsp</th>
-                                <th>&nbsp</th>
-                                <th>&nbsp</th>
-                                <th>&nbsp</th>
-                                <th>&nbsp</th>
-
-
-                                </tr>
+                            <?php } ?>
+                           
+                                
                                 <tr>
                                     <th colspan="2">Total </th>
 
@@ -103,29 +106,15 @@
                                 </tr>
 
                             </tbody>
-                        </tbody>
+                        
                     </table>
                 </div>
-                 <div class="row">
-
-                                    <div class="col-md-5 col-md-offset-1" >
-                                        <div class="form-group">
-                                            <label for="comment"><b>Detailed Comment</b></label>
-                                            <textarea class="form-control" rows="5" style="resize:none" id="comment" name="comment"></textarea>
-                                        </div>
-                                    </div>
-
-                                    <div class="col-md-5 col-md-offset-1" >
-                                        <div class="form-group">
-                                            <label for="summary"><b>Summary Comment</b></label>
-                                            <textarea class="form-control" rows="5" style="resize:none" id="summary"  name="summary"></textarea>
-                                        </div>
-                                    </div>
-
-
-
-                                </div>
+                 
             </div>
+                
         </div>
     </div>
 </div>
+<?php }else{
+                    echo "Data not Found";
+                } ?>
