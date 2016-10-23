680cabf67ceead1b0131a36397de961c32fd47dd
bhomnath <bhomnath@salyani.com.np>
2016-10-23 15:57:40 +0545

    login updated

--- application/views/dashboard/login/login.php
+++ application/views/dashboard/login/login.php
@@ -79,15 +79,9 @@
 						<div class="invalid">
 							 <?php
             $flashMessage = $this->session->flashdata('flashMessage');
-            if (!empty($flashMessage)) {
-                ?>
-                <div class="alert alert-success fade in">
-                    <p style="text-align:center;font-size:18px;"><strong>!!&nbsp;<?php echo $flashMessage; ?> </strong></p>
-                </div>
-                <hr>
-            <?php
-            }
-          
+            if (!empty($flashMessage)) {               
+                 echo $flashMessage;
+            }          
             if (isset($error)) {
                 echo $error;
             }
